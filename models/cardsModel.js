646320c6d402f1284d85efbef55280cf91726d5b
PedroNevesTucano <tucaninhoplays@gmail.com>
2023-02-09 16:47:21 +0000

    add
    add

--- models/cardsModel.js
+++ models/cardsModel.js
@@ -34,6 +34,43 @@ class Card {
             return { status: 500, result: err };
         }
     }
+    static async getById(id) {
+        try {
+            let [dbCards, fields] =
+                await pool.query("Select * from cards where crd_id=?", [id]);
+            if (!dbCards)
+                return { status: 404, result: { msg: "No card found with that identifier" } };
+            let dbCard = dbCards[0];
+            let result = cardFromDB(dbCard);
+            return { status: 200, result: result };
+        } catch (err) {
+            console.log(err);
+            return { status: 500, result: err };
+        }
+    }
+    static async save(newCard) {
+        try {
+            let [dbCards, fields] =
+                await pool.query("Select * from cards where crd_name=?", [newCard.name]);
+            if (dbCards.length)
+                return {
+                    status: 400, result: [{
+                        location: "body", param: "name",
+                        msg: "That name already exists"
+                    }]
+                };
+            let [result] =
+                await pool.query(`Insert into cards (crd_name, crd_img_url, crd_lore, 
+                crd_description, crd_level, crd_cost, crd_timeout, crd_max_usage, crd_type)
+                values (?,?,?,?,?,?,?,?,?)`, [newCard.name, newCard.url, newCard.lore,
+                newCard.description, newCard.level, newCard.cost, newCard.timeout,
+                newCard.maxUsage, newCard.type]);
+            return { status: 200, result: result };
+        } catch (err) {
+            console.log(err);
+            return { status: 500, result: err };
+        }
+    }
 }
 
 module.exports = Card;
