cd90889eb2bf12c7238167ffdb32721c8ef5d33d
PedroNevesTucano <tucaninhoplays@gmail.com>
2023-02-09 17:41:10 +0000

    add
    add

--- routes/cardsRoutes.js
+++ routes/cardsRoutes.js
@@ -63,4 +63,22 @@ router.post("/",
         }
     });
 
+router.get('/filter', async function (req, res, next) {
+    try {
+        console.log("Filter cards");
+        if (req.query.typeId) {
+            let result = await Card.filterByType(req.query.typeId);
+            res.status(result.status).send(result.result);
+        } else if (req.query.descContains) {
+            let result = await Card.filterByLoreOrDescription(req.query.descContains);
+            res.status(result.status).send(result.result);
+        } else {
+            res.status(400).send({ msg: "No filter provided" });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
+});
+
 module.exports = router;
